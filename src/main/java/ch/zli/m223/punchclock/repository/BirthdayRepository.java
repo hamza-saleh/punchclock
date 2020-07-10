8e14e04504d36dad608ab24621cc086704926bef
hamsal1 <hamu14@hotmail.com>
2020-07-10 09:13:49 +0200

    Service

--- src/main/java/ch/zli/m223/punchclock/repository/BirthdayRepository.java
+++ src/main/java/ch/zli/m223/punchclock/repository/BirthdayRepository.java
@@ -1,7 +1,6 @@
 package ch.zli.m223.punchclock.repository;
 
 import ch.zli.m223.punchclock.domain.Birthday;
-import ch.zli.m223.punchclock.domain.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BirthdayRepository extends JpaRepository<Birthday, Long> {
--- src/main/java/ch/zli/m223/punchclock/service/BirthdayService.java
+++ src/main/java/ch/zli/m223/punchclock/service/BirthdayService.java
@@ -1,4 +1,30 @@
 package ch.zli.m223.punchclock.service;
 
-public class BirthdayService {
+import ch.zli.m223.punchclock.domain.Entry;
+import ch.zli.m223.punchclock.repository.EntryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EntryService {
+    private EntryRepository entryRepository;
+
+    public EntryService(EntryRepository entryRepository) {
+        this.entryRepository = entryRepository;
+    }
+
+    public Entry createEntry(Entry entry) {
+        return entryRepository.saveAndFlush(entry);
+    }
+
+    public List<Entry> findAll() {
+        return entryRepository.findAll();
+    }
+
+    public void deleteEntry(long id){entryRepository.deleteById(id);}
+
+    public void  updateEntry(Entry entry) {
+        entryRepository.save(entry);
+    }
 }
