2dcd4d604edbaf85a70a19855c9d45182e215f2f
hamsal1 <hamu14@hotmail.com>
2020-07-10 09:00:54 +0200

    Orders Class

--- src/main/java/ch/zli/m223/punchclock/domain/Orders.java
+++ src/main/java/ch/zli/m223/punchclock/domain/Orders.java
@@ -1,4 +1,18 @@
 package ch.zli.m223.punchclock.domain;
 
 public class Orders {
+    private String food;
+    private long ID;
+
+    public String getFood(){
+        return food;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
 }
